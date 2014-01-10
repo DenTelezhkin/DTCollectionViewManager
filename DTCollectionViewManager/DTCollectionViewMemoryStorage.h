1c7056679c4e1008af7745b2aff2efd146c963f5
Denys Telezhkin <strangervir@gmail.com>
2014-01-10 16:54:27 +0200

    fix documentation for move item method

--- DTCollectionViewManager/DTCollectionViewMemoryStorage.h
+++ DTCollectionViewManager/DTCollectionViewMemoryStorage.h
@@ -14,9 +14,9 @@
 /**
  Move collection item to `indexPath`.
  
- @param item model to move.
+ @param sourceIndexPath source indexPath of item to move.
  
- @param indexPath Index, where item should be moved.
+ @param destinationIndexPath Index, where item should be moved.
  
  @warning Moving item at index, that is not valid, won't do anything, except logging into console about failure
  */
