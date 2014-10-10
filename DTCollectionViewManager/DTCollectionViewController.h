47c85a334171514752ec588bcd7d9e4561001030
Denys Telezhkin <strangervir@gmail.com>
2014-10-10 16:40:30 +0300

    cleanup header

--- DTCollectionViewManager/DTCollectionViewController.h
+++ DTCollectionViewManager/DTCollectionViewController.h
@@ -23,54 +23,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-/**
- `DTCollectionViewController` manages all `UICollectionView` datasource methods and provides API for mapping your data models to UICollectionViewCells. It also contains storage object, that is responsible for providing data models.
- 
- ## Setup
- 
- # General steps
- - You should have custom `UICollectionViewCell` subclasses that manage cell layout, using given data model (or `DTCollectionViewCell`, which is convenience UICollectionView subclass, that conforms to `DTModelTransfer` protocol)
- - Every cell class should be mapped to model class using mapping methods.
- - `UICollectionView` datasource and delegate is your `DTCollectionViewController` subclass.
- 
- ## Managing collection items
- 
- Starting with 2.0, storage classes have been moved to separate project - DTModelStorage. Basically, every change in data storage object is transferred to current controller and automatically properly animated. DTMemoryStorage is a class used by default, you can retrieve it's instance by calling '-memoryStorage' method.
- 
- ## Mapping
- 
- Use `registerCellClass:forModelClass` for mapping cell class to model. 'DTCollectionViewController' will automatically check, if there's a nib with the same name as cellClass. If it is - this nib is registered for modelClass. If there's no nib - cellClass will be registered for modelClass. Cells will be created using `dequeueReusableCellWithReuseIdentifier:forIndexPath:` method.
- 
- For mapping supplementary views, use `registerSupplementaryClass:forKind:forModelClass:` method.
- 
- Before executing mapping methods, make sure that collectionView property is set and collectionView is created. Good spot to call mapping methods is viewDidLoad method.
- 
- ## Search
- 
- Search implementation depends on what data storage you use. In both cases it's recommended to use this class as UISearchBarDelegate. Then searching data storage will be created automatically for every change in UISearchBar.
- 
- # DTMemoryStorage
- 
- Call memoryStorage setSearchingBlock:forModelClass: to determine, whether model of passed class should show for current search criteria. This method can be called as many times as you need.
- 
- # DTCoreDataStorage
- 
- Subclass DTCoreDataStorage and implement single method: -searchingStorageForSearchString:inSearchScope:. You will need to provide a storage with NSFetchedResultsController and appropriate NSPredicate.
- 
- ## Foundation class clusters mapping
- 
- Most of the time you will have your own data models for cells. However, sometimes it's more convenient to use Foundation types, such as NSString, NSNumber, etc. For example, if you have supplementary view - header, that does not have any information except for it's title - you'll probably want to use NSString as its model. Mutable versions are also supported. 
- 
- DTCollectionViewController supports mapping of following Foundation types:
-  - NSString
-  - NSNumber
-  - NSDictionary
-  - NSArray
- */
-
 #import "DTMemoryStorage+DTCollectionViewManagerAdditions.h"
 #import "DTStorage.h"
 #import "DTCollectionViewControllerEvents.h"
+
+/**
+ `DTCollectionViewController` manages all `UICollectionView` datasource methods and provides API for mapping your data models to UICollectionViewCells. It also contains storage object, that is responsible for providing data models.
+ */
 
 @interface DTCollectionViewController : UIViewController <UICollectionViewDataSource,UICollectionViewDelegate, UISearchBarDelegate, DTCollectionViewControllerEvents>
 
