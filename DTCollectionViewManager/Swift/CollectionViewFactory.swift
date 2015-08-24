b80c12e385e360ecb5040df627a3f7288938a8ad
Denys Telezhkin <strangervir@gmail.com>
2015-08-24 14:20:29 +0300

    add registration, UICollectionView datasource and delegate methods

--- DTCollectionViewManager/Swift/CollectionViewFactory.swift
+++ DTCollectionViewManager/Swift/CollectionViewFactory.swift
@@ -69,5 +69,66 @@ extension CollectionViewFactory
         self.addMappingForViewType(.Cell, viewClass: T.self)
     }
     
-//    func registerSupplementaryClass
+    func registerSupplementaryClass<T:ModelTransfer where T:UICollectionReusableView>(supplementaryClass: T.Type, forKind kind: String)
+    {
+        let reuseIdentifier = RuntimeHelper.classNameFromReflection(_reflect(supplementaryClass))
+        if UINib.nibExistsWithNibName(reuseIdentifier, inBundle: bundle) {
+            self.collectionView.registerNib(UINib(nibName: reuseIdentifier, bundle: bundle), forSupplementaryViewOfKind: kind, withReuseIdentifier: reuseIdentifier)
+        }
+        self.addMappingForViewType(ViewType.SupplementaryView(kind: kind), viewClass: T.self)
+    }
+    
+    func registerNibNamed<T:ModelTransfer where T:UICollectionReusableView>(nibName: String, forSupplementaryClass supplementaryClass: T.Type, forKind kind: String)
+    {
+        let reuseIdentifier = RuntimeHelper.classNameFromReflection(_reflect(supplementaryClass))
+        assert(UINib.nibExistsWithNibName(nibName, inBundle: bundle))
+        self.collectionView.registerNib(UINib(nibName: nibName, bundle: bundle), forSupplementaryViewOfKind: kind, withReuseIdentifier: reuseIdentifier)
+        self.addMappingForViewType(ViewType.SupplementaryView(kind: kind), viewClass: T.self)
+    }
+}
+
+// MARK: View creation
+extension CollectionViewFactory
+{
+    func cellForModel(model: Any, atIndexPath indexPath:NSIndexPath) -> UICollectionViewCell
+    {
+        guard let unwrappedModel = RuntimeHelper.recursivelyUnwrapAnyValue(model) else {
+            assertionFailure("Received nil model at indexPath: \(indexPath)")
+            return UICollectionViewCell()
+        }
+        
+        let typeMirror = RuntimeHelper.mirrorFromModel(unwrappedModel)
+        if let mapping = self.mappingForViewType(.Cell, modelTypeMirror: typeMirror)
+        {
+            let cellClassName = RuntimeHelper.classNameFromReflection(mapping.viewTypeMirror)
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellClassName, forIndexPath: indexPath)
+            mapping.updateBlock(cell, unwrappedModel)
+            return cell
+        }
+        
+        assertionFailure("Unable to find cell mappings for type: \(_reflect(typeMirror.valueType).summary)")
+        
+        return UICollectionViewCell()
+    }
+
+    func supplementaryViewOfKind(kind: String, forModel model: Any, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView
+    {
+        guard let unwrappedModel = RuntimeHelper.recursivelyUnwrapAnyValue(model) else {
+            assertionFailure("Received nil model at indexPath: \(indexPath)")
+            return UICollectionViewCell()
+        }
+        
+        let typeMirror = RuntimeHelper.mirrorFromModel(unwrappedModel)
+        if let mapping = self.mappingForViewType(ViewType.SupplementaryView(kind: kind), modelTypeMirror: typeMirror)
+        {
+            let viewClassName = RuntimeHelper.classNameFromReflection(mapping.viewTypeMirror)
+            let reusableView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: viewClassName, forIndexPath: indexPath)
+            mapping.updateBlock(reusableView, unwrappedModel)
+            return reusableView
+        }
+        
+        assertionFailure("Unable to find cell mappings for type: \(_reflect(typeMirror.valueType).summary)")
+        
+        return UICollectionReusableView()
+    }
 }
--- DTCollectionViewManager/Swift/DTCollectionViewManager.swift
+++ DTCollectionViewManager/Swift/DTCollectionViewManager.swift
@@ -51,4 +51,139 @@ public class DTCollectionViewManager : NSObject {
     }
     
     weak var delegate : DTCollectionViewManageable?
+    
+    ///  Factory for creating cells and reusable views for UICollectionView
+    private lazy var viewFactory: CollectionViewFactory = {
+        precondition(self.collectionView != nil, "Please call manager.startManagingWithDelegate(self) before calling any other DTCollectionViewManager methods")
+        return CollectionViewFactory(collectionView: self.collectionView)
+    }()
+    
+    /// Bundle to search your xib's in. This can sometimes be useful for unit-testing. Defaults to NSBundle.mainBundle()
+    public var viewBundle = NSBundle.mainBundle()
+        {
+        didSet {
+            viewFactory.bundle = viewBundle
+        }
+    }
+    
+    /// Implicitly unwrap storage property to `MemoryStorage`.
+    /// - Warning: if storage is not MemoryStorage, will throw an exception.
+    public var memoryStorage : MemoryStorage!
+        {
+            precondition(storage is MemoryStorage, "DTTableViewManager memoryStorage method should be called only if you are using MemoryStorage")
+            
+            return storage as! MemoryStorage
+    }
+    
+    /// Storage, that holds your UICollectionView models. By default, it's `MemoryStorage` instance.
+    /// - Note: When setting custom storage for this property, it will be automatically configured for using with UICollectionViewFlowLayout and it's delegate will be set to `DTCollectionViewManager` instance.
+    /// - Note: Previous storage `delegate` property will be nilled out to avoid collisions.
+    /// - SeeAlso: `MemoryStorage`, `CoreDataStorage`.
+    public var storage : StorageProtocol = {
+        let storage = MemoryStorage()
+        storage.configureForCollectionViewFlowLayoutUsage()
+        return storage
+        }()
+        {
+        willSet {
+            // explicit self is required due to known bug in Swift compiler - https://devforums.apple.com/message/1065306#1065306
+            self.storage.delegate = nil
+        }
+        didSet {
+            if let headerFooterCompatibleStorage = storage as? BaseStorage {
+                headerFooterCompatibleStorage.configureForCollectionViewFlowLayoutUsage()
+            }
+            storage.delegate = self
+        }
+    }
+    
+    /// Call this method before calling any of `DTCollectionViewManager` methods.
+    /// - Precondition: UICollectionView instance on `delegate` should not be nil.
+    /// - Parameter delegate: Object, that has UICollectionView, that will be managed by `DTCollectionViewManager`.
+    public func startManagingWithDelegate(delegate : DTCollectionViewManageable)
+    {
+        precondition(delegate.collectionView != nil,"Call startManagingWithDelegate: method only when UICollectionView has been created")
+        
+        self.delegate = delegate
+        delegate.collectionView.delegate = self
+        delegate.collectionView.dataSource = self
+    }
+}
+
+// MARK: - Runtime forwarding
+extension DTCollectionViewManager
+{
+    /// Any `UICollectionViewDatasource` and `UICollectionViewDelegate` method, that is not implemented by `DTCollectionViewManager` will be redirected to delegate, if it implements it.
+    public override func forwardingTargetForSelector(aSelector: Selector) -> AnyObject? {
+        return delegate
+    }
+    
+    /// Any `UICollectionViewDatasource` and `UICollectionViewDelegate` method, that is not implemented by `DTCollectionViewManager` will be redirected to delegate, if it implements it.
+    public override func respondsToSelector(aSelector: Selector) -> Bool {
+        if self.delegate?.respondsToSelector(aSelector) ?? false {
+            return true
+        }
+        return super.respondsToSelector(aSelector)
+    }
+}
+
+extension DTCollectionViewManager : UICollectionViewDataSource
+{
+    public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return storage.sections[section].numberOfObjects
+    }
+    
+    public func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return storage.sections.count
+    }
+    
+    public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let model = storage.objectAtIndexPath(indexPath)
+        let cell = viewFactory.cellForModel(model, atIndexPath: indexPath)
+        return cell
+    }
+    
+    public func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView
+    {
+        if let model = (self.storage as? SupplementaryStorageProtocol)?.supplementaryModelOfKind(kind, sectionIndex: indexPath.section) {
+            let view = viewFactory.supplementaryViewOfKind(kind, forModel: model, atIndexPath: indexPath)
+            return view
+        }
+        return UICollectionReusableView()
+    }
+}
+
+extension DTCollectionViewManager : UICollectionViewDelegateFlowLayout
+{
+    public func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize
+    {
+        if let size = (self.delegate as? UICollectionViewDelegateFlowLayout)?.collectionView?(collectionView, layout: collectionViewLayout, referenceSizeForHeaderInSection: section) {
+            return size
+        }
+        if let _ = (storage as? SupplementaryStorageProtocol)?.supplementaryModelOfKind(UICollectionElementKindSectionHeader, sectionIndex: section) {
+            return (collectionViewLayout as! UICollectionViewFlowLayout).headerReferenceSize
+        }
+        return CGSizeZero
+    }
+    
+    public func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if let size = (self.delegate as? UICollectionViewDelegateFlowLayout)?.collectionView?(collectionView, layout: collectionViewLayout, referenceSizeForFooterInSection: section) {
+            return size
+        }
+        if let _ = (storage as? SupplementaryStorageProtocol)?.supplementaryModelOfKind(UICollectionElementKindSectionFooter, sectionIndex: section) {
+            return (collectionViewLayout as! UICollectionViewFlowLayout).footerReferenceSize
+        }
+        return CGSizeZero
+    }
+}
+
+extension DTCollectionViewManager : StorageUpdating
+{
+    public func storageDidPerformUpdate(update: StorageUpdate) {
+        
+    }
+    
+    public func storageNeedsReloading() {
+        
+    }
 }
