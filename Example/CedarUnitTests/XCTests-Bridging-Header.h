37bd51d86e00e02a5e35bd68f05433416616af7d
Denys Telezhkin <strangervir@gmail.com>
2015-09-06 16:23:49 +0300

    remove DTCollectionViewManager 3.x

--- Example/CedarUnitTests/XCTests-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import "DTModelStorage.h"
-#import "DTCollectionViewController.h"
